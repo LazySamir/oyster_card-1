e2ed562940eab650c743b435a3cbae5c73dee125
Samir Soormally <samirsoormally@gmail.com>
2018-08-16 15:40:52 +0100

    refactored

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,24 +1,33 @@
 require 'station'
 describe Station do
-let(:station1)  { described_class.new(station, zone) }
-let(:station)   { double :station }
-let(:zone)      { double :zone }
+  subject {described_class.new(name: "Aldgate", zone: 1)}
 
-  describe "#initialize" do
-    it "text" do
-      expect(Station).to respond_to(:new).with(2).arguments
-    end
+# let(:station1)  { described_class.new(name, zone) }
+# let(:station)   { double :station }
+# let(:zone)      { double :zone }
 
-    describe ".name" do
-      it "assigns argument" do
-        expect(station1.name).to eq(station)
-      end
-    end
+  # describe "#initialize" do
+  #   it "text" do
+  #     expect(Station).to respond_to(:new).with(2).arguments
+  #   end
 
-    describe ".zone" do
-      it "assigns argument" do
-        expect(station1.zone).to eq(zone)
-      end
-    end
+  it "knows its name" do
+    expect(subject.name).to eq("Aldgate")
   end
+
+  it "knows its zone" do
+    expect(subject.zone).to eq(1)
+  end
+
+    # describe ".name" do
+    #   it "assigns argument" do
+    #     expect(station1.name).to eq(station)
+    #   end
+    # end
+    #
+    # describe ".zone" do
+    #   it "assigns argument" do
+    #     expect(station1.zone).to eq(zone)
+    #   end
+    # en
 end
